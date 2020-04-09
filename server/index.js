eef1a72a18415ed5cc383a247da96388b912674b
pizzinatto <pizzinatto22@gmail.com>
2020-04-09 18:22:41 -0300

    Transformando servidor para respostas HTTP

--- server/index.js
+++ server/index.js
@@ -1,14 +1,47 @@
 const servidores = require("./servidores");
 const alunos = require("./graduacao");
 const pos = require("./pos_graduacao");
+const http = require('http');
+const moment = require('moment');
 
+let cache = {};
+let horaCache = null;
 
-(async () => {
-  const dados = {
-    servidores: await servidores(),
-    graduacao: await alunos(),
-    pos: await pos(),
-  };
+async function requestListener (req, res) {
+  const agora = new Date();
 
-  console.log("acabou com: ", dados);
-})();
+  if (!horaCache || (agora.getTime() - horaCache.getTime() >= 1000*60*5)) {
+    horaCache = agora;
+
+    try {
+      cache.servidores = await servidores();
+    } catch(e) {
+      cache.servidores = {};
+    }
+
+    try {
+      cache.graduacao = await alunos();
+
+    } catch (e) {
+      cache.graduacao = {}
+    }
+
+    try {
+      cache.pos = await pos();
+    } catch(e) {
+      cache.pos = {}
+    }
+
+    cache.hora = moment(horaCache).format("DD/MM/YYYY HH:mm:ss")
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  res.end(JSON.stringify(cache));
+}
+
+const server = http.createServer(requestListener);
+server.listen(3002);
--- server/servidores.js
+++ server/servidores.js
@@ -22,34 +22,34 @@ module.exports = () => {
     .then(listaServidores => {
 
       let dados = {
-        outros: [],
-        professores: [],
-        tecnicos: [],
-        setores: [],
+        outros: {},
+        professores: {},
+        tecnicos: {},
+        setores: {},
       }
 
       for (let s of listaServidores) {
         const setor = s.setor;
         const cargo = s.cargo;
 
-        dados.setores[setor] = dados.setores[setor] || [];
+        dados.setores[setor] = dados.setores[setor] || {};
 
 
         if (s.cargo.match(/indefinido/i)) {
           computar(dados.outros, cargo);
 
-          dados.setores[setor].outros = dados.setores[setor].outros || [];
+          dados.setores[setor].outros = dados.setores[setor].outros || {};
           computar(dados.setores[setor].outros, cargo);
 
         } else if (s.cargo.match(/professor/i)) {
           computar(dados.professores, cargo);
 
-          dados.setores[setor].professores = dados.setores[setor].professores || [];
+          dados.setores[setor].professores = dados.setores[setor].professores || {};
           computar(dados.setores[setor].professores, cargo);
         } else {
           computar(dados.tecnicos, cargo);
 
-          dados.setores[setor].tecnicos = dados.setores[setor].tecnicos || [];
+          dados.setores[setor].tecnicos = dados.setores[setor].tecnicos || {};
           computar(dados.setores[setor].tecnicos, cargo);
         }
 
