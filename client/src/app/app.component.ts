1a60db1968215866ff6e1afd37e6f413992df952
pizzinatto <pizzinatto22@gmail.com>
2020-05-12 14:23:39 -0300

    Alterada a porta e URL quando está em produção

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { KeyValue } from '@angular/common';
+import { isDevMode } from '@angular/core';
+
 
 
 @Component({
@@ -13,7 +15,13 @@ export class AppComponent {
   dados = null;
 
   constructor(private http: HttpClient) {
-    const url = window.location.protocol + "//" + window.location.hostname + ":3002/" ;
+    let url;
+    if (isDevMode()) {
+      url = window.location.protocol + "//" + window.location.hostname + ":3002/" ;
+    } else {
+      url = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/api";
+    }
+
     this.http.get(url).subscribe(res => {
       this.dados = res;
     })
